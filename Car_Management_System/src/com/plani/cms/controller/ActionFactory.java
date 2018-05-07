fcd092670ca4a2b96e50c82313e8a03de06333a5
kayh45 <kayh4545@gmail.com>
2018-05-07 23:16:16 +0900

    부서 등록화면 추가

--- Car_Management_System/src/com/plani/cms/controller/ActionFactory.java
+++ Car_Management_System/src/com/plani/cms/controller/ActionFactory.java
@@ -2,6 +2,7 @@ package com.plani.cms.controller;
 
 import com.plani.cms.controller.action.Action;
 import com.plani.cms.controller.action.MainAction;
+import com.plani.cms.controller.action.member.DeptWriteFormAction;
 import com.plani.cms.controller.action.member.MemberWriteFormAction;
 
 public class ActionFactory {
@@ -27,6 +28,8 @@ public class ActionFactory {
 			action = new MainAction();
 		} else if (command.equals("member_write_form")) {
 			action = new MemberWriteFormAction();
+		} else if (command.equals("dept_write_form")) {
+			action = new DeptWriteFormAction();
 		}
 		
 		return action;
