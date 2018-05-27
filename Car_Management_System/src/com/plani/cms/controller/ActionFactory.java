a63ecdf39b669dba3b498b1f8cbe30e4512c6fb1
csc9034 <csc9034@naver.com>
2018-05-27 17:40:37 +0900

    팩토리 오류 수정

--- Car_Management_System/src/com/plani/cms/controller/ActionFactory.java
+++ Car_Management_System/src/com/plani/cms/controller/ActionFactory.java
@@ -131,7 +131,6 @@ public class ActionFactory {
 			action = new PlaceWriteFormAction();
 		} else if (command.equals("reserve_write_form")) {
 			action = new ReserveWriteFormAction();
-<<<<<<< HEAD
 		}/* 정비내역 등록 구현 start*/
 		 else if (command.equals("repa_write")) {
 				action = new RepaWriteAction();
@@ -143,16 +142,7 @@ public class ActionFactory {
 				action = new RepaCarWriteCheckFormAction();
 			}
 
-=======
-		} /* 정비내역 등록 구현 start */
-		else if (command.equals("repa_write")) {
-			action = new RepaWriteAction();
-		} else if (command.equals("repa_write_form")) {
-			action = new RepaWriteFormAction();
-		} else if (command.equals("repa_write_check_form")) {
-			action = new RepaWriteCheckFormAction();
-		}
->>>>>>> branch 'master' of https://github.com/kayh45/G.IT
+	
 
 		return action;
 
