185b7a7179e5f43e3df2ffaf1f15b765cc7d37f0
kayh45 <kayh4545@gmail.com>
2018-06-07 04:44:45 +0900

    시뮬레이션 로직은 어느정도 완성

--- Car_Management_System/src/com/plani/cms/controller/action/carlog/CarlogAutoWriteNextAction.java
+++ Car_Management_System/src/com/plani/cms/controller/action/carlog/CarlogAutoWriteNextAction.java
@@ -1,7 +1,10 @@
 package com.plani.cms.controller.action.carlog;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,14 +20,14 @@ public class CarlogAutoWriteNextAction implements Action{
 		String url = "index.jsp";
 		
 		String car_reg_no = request.getParameter("car_usable_no");
-		String carlog_year = request.getParameter("car_reg_no");
-		String carlog_month = request.getParameter("car_reg_no");
+		String carlog_year = request.getParameter("carlog_year");
+		String carlog_month = request.getParameter("carlog_month");
 		String mem_id = request.getParameter("mem_id");
 		int avg_oil = Integer.parseInt(request.getParameter("avg_oil"));
 		int rate_oil = Integer.parseInt(request.getParameter("rate_oil"));
 		String cour_no = request.getParameter("cour_no");
 		String driv_purpo = request.getParameter("driv_purpo");
-		String distance = request.getParameter("distance");
+		int distance = Integer.parseInt(request.getParameter("distance"));
 		
 		String[] card_day = request.getParameterValues("card_day");		
 		String[] card_oil = request.getParameterValues("card_oil");		
@@ -55,11 +58,101 @@ public class CarlogAutoWriteNextAction implements Action{
 		total_dist += (oil/avg_oil*rate_oil); // 유류비÷평균유가*연비　
 		total_dist += (tol*0.02); // 톨비*0.02 (킬로당 50원으로 상정)
 		total_dist *= 0.8;
-		System.out.println("총 주행거리 : " + total_dist);
-		total_dist -= (tol*0.02);
-		System.out.println("총 주행거리(톨비 제외) : " + total_dist);
-								
+		System.out.println("총 주행거리 : " + total_dist);		
 		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Integer.parseInt(carlog_year), Integer.parseInt(carlog_month), 1);
+		int endDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int workday = 0;
+		
+		for(int i = 1; i <= endDay; i++) {
+			int week = cal.get(Calendar.DAY_OF_WEEK);
+			if (!(week == 1 || week == 7)) {
+				workday++;
+			}
+		}
+		
+		int remainDist = 0;
+		if((total_dist/distance)>workday) {
+			remainDist = total_dist - (distance * workday);
+		}
+		
+		int[][] carlogArr = new int[5][32]; // 일자,거리,유류비,교통비,경로번호
+		for(int i = 1; i <= endDay; i++) {
+			boolean notTol = false;
+			boolean isOil = false;
+			int tol_fee = 0;
+			int oil_fee = 0;
+			int tol_cour = 0;
+			for(int j = 0; j < card_arr[0].length; j++) {
+				if(i == card_arr[0][j]) {
+					if(card_arr[2][j] != 0) {
+						notTol = false;
+						isOil = false;
+						tol_fee = card_arr[2][j];
+						tol_cour = card_arr[3][j];
+					}else if(card_arr[1][j] != 0){
+						notTol = true;
+						isOil = true;
+						oil_fee = card_arr[1][j];
+					}
+				}else {
+					notTol = true;
+					isOil = false;
+				}
+			}
+			cal.set(Integer.parseInt(carlog_year), Integer.parseInt(carlog_month), i);
+			int week = cal.get(Calendar.DAY_OF_WEEK);
+			if (!(week == 1 || week == 7)) {
+				Random rnd = new Random();
+				int rndNo = rnd.nextInt(5);
+				int dayJump = 0;
+				if(remainDist<0){
+					dayJump = rnd.nextInt(-remainDist/200);
+				}
+				System.out.println(i);
+				carlogArr[0][i] = i;
+				System.out.println(total_dist + ", " + notTol + ", " + isOil);
+				
+				if(total_dist >= 0) {
+					if(notTol) {
+						if(!isOil) {
+							if((remainDist-rndNo)>=0) {
+								remainDist -= rndNo;
+								carlogArr[1][i] = distance + rndNo;
+							}else {
+								carlogArr[1][i] = distance;
+							}
+						}else {
+							if((remainDist-(rndNo*2))>=0) {
+								remainDist -= rndNo;
+								carlogArr[1][i] = distance + (rndNo*2);
+							}else {
+								carlogArr[1][i] = distance + 2;
+							}
+						}
+					}else {
+						carlogArr[1][i] = (int)(tol_fee * 0.02);
+					}
+					total_dist -= carlogArr[1][i];
+				}else {
+					carlogArr[1][i] = 0;
+				}								
+				carlogArr[2][i] = oil_fee;
+				carlogArr[3][i] = tol_fee;
+				carlogArr[4][i] = tol_cour;
+
+					i += dayJump;
+
+			}
+			
+		}
+		
+			for(int j = 1; j < carlogArr[0].length; j++) {
+				System.out.print("일자: " + carlogArr[0][j] + ", 거리:  " + carlogArr[1][j]);
+				System.out.println();
+			}
+			
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
 	}
